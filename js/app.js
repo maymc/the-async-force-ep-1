a0ac21acc4f13da3cfe703a23aec9441e1c67774
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-29 16:10:00 -1000

    Completed problem 1 with person 4 name and homeworld

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
 console.log("sanity check");
 
+//Problem 1: Person 4
 let oReq = new XMLHttpRequest();
 
 oReq.addEventListener("load", function(res) {
-  console.log("response", JSON.parse(res.currentTarget.response));
+  //check response
+  console.log("response: ", JSON.parse(res.currentTarget.response));
+
+  //person 4 name
+  console.log(
+    "response person4Name: ",
+    JSON.parse(res.currentTarget.response).name
+  );
+
+  document.getElementById("person4Name").innerHTML = JSON.parse(
+    res.currentTarget.response
+  ).name;
+
+  //HomeWorld is on a different link so it needs to be on a different request.
+  //person 4 home world
+  oReq = new XMLHttpRequest();
+
+  oReq.addEventListener("load", function(res) {
+    console.log(
+      "response person4HomeWorld: ",
+      JSON.parse(res.currentTarget.response).name
+    );
+    document.getElementById("person4HomeWorld").innerHTML = JSON.parse(
+      res.currentTarget.response
+    ).name;
+  });
+
+  oReq.open("GET", "https://swapi.co/api/planets/1/");
+  oReq.send();
 });
 
 oReq.open("GET", "https://swapi.co/api/people/4/");
