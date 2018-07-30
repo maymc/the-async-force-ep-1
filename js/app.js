9a525a7d4b51a275b3d49f5f3dc222fbe8115fa9
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-29 18:10:31 -1000

    Fixed the list element as it was not appended the first time. Test planet is showing up properly. Need to get the rest of the planets to show up.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ console.log("sanity check");
 //Problem 1: Person 4
 let oReq = new XMLHttpRequest();
 
-oReq.addEventListener("load", function(res) {
+oReq.addEventListener("load", function (res) {
   //check response
   console.log("response: ", JSON.parse(res.currentTarget.response));
 
@@ -21,7 +21,7 @@ oReq.addEventListener("load", function(res) {
   //person 4 home world
   oReq = new XMLHttpRequest();
 
-  oReq.addEventListener("load", function(res) {
+  oReq.addEventListener("load", function (res) {
     console.log(
       "response person4HomeWorld: ",
       JSON.parse(res.currentTarget.response).name
@@ -41,7 +41,7 @@ oReq.send();
 //Problem 2:
 let o2Req = new XMLHttpRequest();
 
-o2Req.addEventListener("load", function(res) {
+o2Req.addEventListener("load", function (res) {
   //Check response
   console.log("response: ", JSON.parse(res.currentTarget.response));
 
@@ -58,7 +58,7 @@ o2Req.addEventListener("load", function(res) {
   //person 14 species
   o2Req = new XMLHttpRequest();
 
-  o2Req.addEventListener("load", function(res) {
+  o2Req.addEventListener("load", function (res) {
     console.log(
       "response person14Species: ",
       JSON.parse(res.currentTarget.response).name
@@ -79,19 +79,13 @@ o2Req.send();
 //Problem 3:
 o3Req = new XMLHttpRequest();
 
-o3Req.addEventListener("load", function(res) {
-  console.log(
-    "response films: ",
-    JSON.parse(res.currentTarget.response).results
-  );
+o3Req.addEventListener("load", function (res) {
+  console.log("response films: ", JSON.parse(res.currentTarget.response).results);
 
   //Create list elements and add them to film list
   let filmListElem = document.getElementById("filmList");
-  for (
-    let i = 0;
-    i < JSON.parse(res.currentTarget.response).results.length;
-    i++
-  ) {
+
+  for (let i = 0; i < JSON.parse(res.currentTarget.response).results.length; i++) {
     //Create list element called list
     let film = document.createElement("li");
     film.className = "film";
@@ -99,9 +93,7 @@ o3Req.addEventListener("load", function(res) {
     //Create film title element for each film with heading size 2
     let filmTitle = document.createElement("h2");
     filmTitle.className = "filmTitle";
-    filmTitle.innerHTML = JSON.parse(res.currentTarget.response).results[
-      i
-    ].title;
+    filmTitle.innerHTML = JSON.parse(res.currentTarget.response).results[i].title;
     film.appendChild(filmTitle);
 
     //Create film's planets list
@@ -119,26 +111,33 @@ o3Req.addEventListener("load", function(res) {
     planet.className = "planet";
     filmPlanets.appendChild(planet);
 
-    //Create a planet name title element for each planet with heading4
-    let planetName = document.createElement("h4");
-    planetName.className = "planetName";
+    o4Req = new XMLHttpRequest();
 
-    o3Req = new XMLHttpRequest();
-
-    o3Req.addEventListener("load", function(res) {
+    o4Req.addEventListener("load", function (res) {
       console.log("response planet: ", JSON.parse(res.currentTarget.response));
+
+      //Create a planet name title element for each planet with heading4
+      let planetName = document.createElement("h4");
+      planetName.className = "planetName";
+      planetName.innerHTML = JSON.parse(res.currentTarget.response).name;
+      planet.appendChild(planetName);
     });
-    for (
-      let j = 0;
-      j < JSON.parse(res.currentTarget.response).results[i].planets;
-      j++
-    ) {
-      o3Req.open(
-        "GET",
-        JSON.parse(res.currentTarget.response).results[i].planets[j]
-      );
-      o3Req.send();
-    }
+
+    o4Req.open("GET", "https://swapi.co/api/planets/2/");
+    o4Req.send();
+
+    // for (let j = 0; j < JSON.parse(res.currentTarget.response).results[i].planets.length; j++) {
+    //   var filmName = JSON.parse(res.currentTarget.response).results[i].title;
+    //   var someshit = JSON.parse(res.currentTarget.response).results[i].planets[j];
+    //   console.log("film: " + filmName + ", planet: " + someshit);
+    //   o4Req.open("GET", someshit);
+
+    //   o4Req.addEventListener("load", function (res) {
+    //     console.log("response planet: ", JSON.parse(res.currentTarget.response).name);
+    //     planetName.innerHTML = JSON.parse(res.currentTarget.response).name;
+    //   });
+    //   o4Req.send();
+    // }
 
     filmListElem.appendChild(film);
   }
